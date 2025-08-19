833e28b9db9c5e4849dbccf73753883900eb4bab
Guilherme-Bueno-Costa <guilherme.buenoc23@gmail.com>
2025-08-19 11:52:25 -0300

    feat: move get tasksById, updateTasks, createTask and deleteTask to controller

--- back-end/scr/controllers/task.controller.js
+++ back-end/scr/controllers/task.controller.js
@@ -1,16 +1,86 @@
 const TaskModel = require("../models/task.model");
 
-//
+// Classe para definir as regras de negócio das rotas de requisição
 class TaskController {
   constructor(req, res) {
     this.req = req;
     this.res = res;
   }
 
+  //  Método para encontrar todas as tasks do bando de dados
   async getTasks() {
     try {
       const tasks = await TaskModel.find({});
       this.res.status(200).send(tasks);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+  // Método para exibir uma task especifica
+  async showTask() {
+    try {
+      const taskId = this.req.params.id;
+
+      const task = await TaskModel.findById(taskId);
+
+      if (!task) {
+        return this.res.status(404).send("Essa tarefa não foi encontrado");
+      }
+
+      this.res.status(200).send(task);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  //   Método parra criar uma task nova e salvar no banco de dados
+  async createTask() {
+    try {
+      const newTask = new TaskModel(this.req.body);
+
+      await newTask.save();
+
+      this.res.status(201).send(newTask);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  //   Método para atualizar uma task
+  async updateTask() {
+    try {
+      const taskId = this.req.params.id;
+      const taskData = this.req.body;
+
+      const taskToUpdate = await TaskModel.findById(taskId);
+      if (!taskToUpdate) {
+        return this.res.status(404).send("Essa tarefa não foi encontrada");
+      }
+      const updatedTask = await TaskModel.findByIdAndUpdate(taskId, taskData, {
+        new: true,
+      });
+
+      this.res.status(200).send(updatedTask);
+    } catch (error) {
+      this.res.status(500).send(error.message);
+    }
+  }
+
+  //   Método para deletar uma task
+  async deleteTask() {
+    try {
+      const taskId = this.req.params.id;
+
+      const taskToDelete = await TaskModel.findById(taskId);
+
+      if (!taskToDelete) {
+        return this.res.status(404).send("Essa tarefa não foi encontrada!");
+      }
+      const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+      this.res
+        .status(200)
+        .send(`A tarefa ${deletedTask.description} foi deletada`);
     } catch (error) {
       this.res.status(500).send(error.message);
     }
--- back-end/scr/router/tasks.routes.js
+++ back-end/scr/router/tasks.routes.js
@@ -12,70 +12,22 @@ router.get("/", async (req, res) => {
 
 // Define uma rota GET para exibir uma determinada task
 router.get("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-
-    const task = await TaskModel.findById(taskId);
-
-    if (!task) {
-      return res.status(404).send("Essa tarefa não foi encontrado");
-    }
-
-    res.status(200).send(task);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).showTask();
 });
 
 // Define uma rota POST para criar novas tasks e salvar no banco de dados
 router.post("/", async (req, res) => {
-  try {
-    const newTask = new TaskModel(req.body);
-
-    await newTask.save();
-
-    res.status(201).send(newTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).createTask();
 });
 
 // Define rota PUT para atualizar uma task
 router.patch("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-    const taskData = req.body;
-
-    const taskToUpdate = await TaskModel.findById(taskId);
-    if (!taskToUpdate) {
-      return res.status(404).send("Essa tarefa não foi encontrada");
-    }
-    const updatedTask = await TaskModel.findByIdAndUpdate(taskId, taskData, {
-      new: true,
-    });
-
-    res.status(200).send(updatedTask);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).updateTask();
 });
 
 // Define rota DELETE para deletar uma task
 router.delete("/:id", async (req, res) => {
-  try {
-    const taskId = req.params.id;
-
-    const taskToDelete = await TaskModel.findById(taskId);
-
-    if (!taskToDelete) {
-      return res.status(404).send("Essa tarefa não foi encontrada!");
-    }
-    const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-
-    res.status(200).send(`A tarefa ${deletedTask.description} foi deletada`);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+  return new TaskController(req, res).deleteTask();
 });
 
 module.exports = router;
