6c426f1726127009ca3a300414caba958656a7b2
Guilherme-Bueno-Costa <guilherme.buenoc23@gmail.com>
2025-08-20 15:38:47 -0300

    refector: change class to components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,69 +1,70 @@
-import React from "react";
-import TaskItem from "./components/TaskItem";
+import { useState } from "react";
+import TaskItem from "./components/TaskItem.js";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleStateChange = this.handleStateChange.bind(this);
-        this.state = {
-            tasks: [
-                {
-                    id: "1",
-                    description: "Estudar programação",
-                    isCompleted: false,
-                },
-                {
-                    id: "2",
-                    description: "Ler",
-                    isCompleted: true,
-                },
-            ],
-        };
-    }
-
-    handleStateChange() {
-        this.setState({
-            tasks: [],
-        });
-    }
-
-    render() {
-        return (
-            <>
-                {this.state.tasks.map((task) => (
-                    <TaskItem key={task.id} task={task} />
-                ))}
-                <button onClick={this.handleStateChange}>Limpar tarefas</button>
-            </>
-        );
-    }
-}
+const App = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: "1",
+            description: "Estudar programação",
+            isCompleted: false,
+        },
+        {
+            id: "2",
+            description: "Ler",
+            isCompleted: true,
+        },
+    ]);
+    return (
+        <>
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
+        </>
+    );
+};
 
 export default App;
 
-// import { useState } from "react";
-// import TaskItem from "./components/TaskItem.js";
+// Para usar componentes de classe segue exemplo abaixo
+// import React from "react";
+// import TaskItem from "./components/TaskItem";
 
-// const App = () => {
-//     const [tasks, setTasks] = useState([
-//         {
-//             id: "1",
-//             description: "Estudar programação",
-//             isCompleted: false,
-//         },
-//         {
-//             id: "2",
-//             description: "Ler",
-//             isCompleted: true,
-//         },
-//     ]);
-//     return (
-//         <>
-//             {tasks.map((task) => (
-//                 <TaskItem key={task.id} task={task} />
-//             ))}
-//         </>
-//     );
-// };
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleStateChange = this.handleStateChange.bind(this);
+//         this.state = {
+//             tasks: [
+//                 {
+//                     id: "1",
+//                     description: "Estudar programação",
+//                     isCompleted: false,
+//                 },
+//                 {
+//                     id: "2",
+//                     description: "Ler",
+//                     isCompleted: true,
+//                 },
+//             ],
+//         };
+//     }
+
+//     handleStateChange() {
+//         this.setState({
+//             tasks: [],
+//         });
+//     }
+
+//     render() {
+//         return (
+//             <>
+//                 {this.state.tasks.map((task) => (
+//                     <TaskItem key={task.id} task={task} />
+//                 ))}
+//                 <button onClick={this.handleStateChange}>Limpar tarefas</button>
+//             </>
+//         );
+//     }
+// }
 
 // export default App;
--- front-end/src/components/TaskItem.js
+++ front-end/src/components/TaskItem.js
@@ -1,28 +1,29 @@
-// const TaskItem = (props) => {
-//     // da pra fazer destructuring do props (usando {task}), aí tira o props do h1
-//     return (
-//         <>
-//             <h1>{props.task.description}</h1>
-//             <p>{props.task.isCompleted ? "Finalizada" : "Não finalizada"}</p>
-//         </>
-//     );
-// };
-
-// export default TaskItem;
-// Mesmo que module.exports = TasItem
-
-import React from "react";
-class TaskItem extends React.Component {
-    render() {
-        const { task } = this.props;
-
-        return (
-            <>
-                <h1>{task.description}</h1>
-                <p>{task.isCompleted ? "Finalizado" : "Não finalizado"}</p>
-            </>
-        );
-    }
-}
+const TaskItem = (props) => {
+    // da pra fazer destructuring do props (usando {task}), aí tira o props do h1
+    return (
+        <>
+            <h1>{props.task.description}</h1>
+            <p>{props.task.isCompleted ? "Finalizada" : "Não finalizada"}</p>
+        </>
+    );
+};
 
 export default TaskItem;
+// Mesmo que module.exports = TaskItem
+
+// Para usar Componentes de Classe segue abaixo exemplo
+// import React from "react";
+// class TaskItem extends React.Component {
+//     render() {
+//         const { task } = this.props;
+
+//         return (
+//             <>
+//                 <h1>{task.description}</h1>
+//                 <p>{task.isCompleted ? "Finalizado" : "Não finalizado"}</p>
+//             </>
+//         );
+//     }
+// }
+
+// export default TaskItem;
