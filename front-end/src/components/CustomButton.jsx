29083171b828dece99dbcc5c634ddf9eaa927575
Guilherme-Bueno-Costa <guilherme.buenoc23@gmail.com>
2025-08-23 15:15:33 -0300

    feat: add CustomButton component

---
+++ front-end/src/components/CustomButton.jsx
@@ -0,0 +1,12 @@
+// Import de arquivos
+import "./CustomButton.scss";
+
+const CustomButton = ({ onClick, children }) => {
+    return (
+        <div className="custom-button-container" onClick={onClick}>
+            {children}
+        </div>
+    );
+};
+
+export default CustomButton;
