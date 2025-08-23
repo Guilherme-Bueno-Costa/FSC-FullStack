9a88f33a6d423e1f2c84e563f955c87ca9f8a88d
Guilherme-Bueno-Costa <guilherme.buenoc23@gmail.com>
2025-08-23 17:24:24 -0300

    feat: fetch Tasks after addition

--- front-end/src/components/AddTask.jsx
+++ front-end/src/components/AddTask.jsx
@@ -1,17 +1,45 @@
 // Import de dependências
 import { useState } from "react";
+import { FaPlus } from "react-icons/fa";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 // Import de functions
 import CustomInput from "./CustomInput";
+import CustomButton from "./CustomButton";
 
 // Import de arquivos
 import "./AddTask.scss";
 
-const AddTask = () => {
+const AddTask = ({ fetchTasks }) => {
+    // Hooks
     const [task, setTask] = useState("");
 
+    // Metodos
     const onChange = (e) => {
         setTask(e.target.value);
+    };
+
+    const handleTaskAddition = async () => {
+        try {
+            if (task.length === 0) {
+                return toast.error(
+                    "É preciso inserir uma descrição para adicionar uma tarefa!"
+                );
+            }
+
+            await axios.post("https://fsc-fullstack.onrender.com/tasks", {
+                description: task,
+                isCompleted: false,
+            });
+            toast.success("Tarefa adicionada com sucesso!");
+
+            await fetchTasks();
+
+            setTask("");
+        } catch (error) {
+            return toast.error("Algo deu errado");
+        }
     };
 
     return (
@@ -22,6 +50,9 @@ const AddTask = () => {
                     value={task}
                     onChange={onChange}
                 />
+                <CustomButton onClick={handleTaskAddition}>
+                    <FaPlus size={14} color="#ffffff" />
+                </CustomButton>
             </div>
         </div>
     );
