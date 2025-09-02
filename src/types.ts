18d9610ca82399230fd42b9dd4c5522265f65282
Guilherme-Bueno-Costa <guilherme.buenoc23@gmail.com>
2025-09-02 10:30:01 -0300

    feat: update type.ts

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,40 @@ const anyValues: any[] = [1, "a", true];
 
 // Tuples (tuplas)
 const person: [number, string, boolean] = [1, "Shadow", false];
+const employees: [number, string, boolean][] = [
+    [1, "Shadow", false],
+    [2, "Light", true],
+];
+
+// Union (forma, não tipagem)
+const uid: number | string = 5;
+
+// Enums
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+const direction1 = Direction.Left;
+console.log(direction1); // => 2 pois o left está na posição 2 do objeto, mas pode assinalar valores  os itens do objeto
+
+// Type Assertion
+const cid: any = 1;
+const customerId = cid as string;
+
+const customerIds = ["1", "2", "3"];
+let uuid = "123";
+
+// Functions
+const sum = (sum1: number, sum2: number): number => {
+    return sum1 + sum2;
+};
+
+sum(2, 6);
+
+// Void
+const printMessage = (message: string) => {
+    console.log(message); // Quando uma função não retorna nada ela é classificada como type void. Podemos aplicar o :void para garantir que ela nao retorne nada se esse for o objetivo
+};
