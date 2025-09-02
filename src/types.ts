ca95d793b3b393b90272b5f05ab796607159eb48
Guilherme-Bueno-Costa <guilherme.buenoc23@gmail.com>
2025-09-02 09:55:36 -0300

    feat: initial settings and types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+// Tipos básicos
+const id: number = 10;
+const course: string = "Full Stack Club";
+const isPublished: boolean = false;
+
+let id2: any; //anula a tipagem do typescript
+
+// Arrays (listas)
+const numbers: number[] = [1, 2, 3, 4, 5];
+const strings: string[] = ["a", "b", "c"];
+const boolenas: boolean[] = [true, false];
+const anyValues: any[] = [1, "a", true];
+
+// Tuples (tuplas)
+const person: [number, string, boolean] = [1, "Shadow", false];
